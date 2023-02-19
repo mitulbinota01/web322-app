e68a70e7a098fe8b616d9a19d9b5a8cb02ca58fb
Mitul Binota <mitulbinota@gmail.com>
2023-02-19 15:19:34 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /***************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Mitul Binota, Student ID: 168112217,  Date: 03-02-2023
+*  Name: Mitul Binota, Student ID: 168112217,  Date: 19-02-2023
 *
 *  Online (Cyclic) Link: https://ochre-newt-hem.cyclic.app 
 ****************************/ 
